dfc39091331db52da3514db2ec8fb125f2c6ab42
Hossam-Hawary <hossameldeen.hawary@gmail.com>
2017-10-11 01:11:03 +0200

    ionic clude Error Monitoring

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,25 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { IonicStorageModule, Storage } from '@ionic/storage';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { IonicApp, IonicModule } from 'ionic-angular';
 
 import { Items } from '../mocks/providers/items';
 import { Settings } from '../providers/providers';
 import { User } from '../providers/providers';
 import { Api } from '../providers/providers';
 import { MyApp } from './app.component';
+import { Pro } from '@ionic/pro';
+
+const IonicPro = Pro.init('ae03d68c', {
+  appVersion: "0.0.1"
+});
+
+//ionic clude Error Monitoring
+export class MyErrorHandler implements ErrorHandler {
+  handleError(err: any): void {
+    IonicPro.monitoring.handleNewError(err);
+  }
+}
 
 // The translate loader needs to know where to load i18n files
 // in Ionic's static asset pipeline.
@@ -66,7 +78,7 @@ export function provideSettings(storage: Storage) {
     StatusBar,
     { provide: Settings, useFactory: provideSettings, deps: [Storage] },
     // Keep this to enable Ionic's runtime error handling during development
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    { provide: ErrorHandler, useClass: MyErrorHandler }
   ]
 })
 export class AppModule { }
